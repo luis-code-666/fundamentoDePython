2068bc7f236771eeca6f351997032e717ca9ff5c
rockband_666 <rockband_666@hotmail.com>
2023-12-06 10:03:51 -0400

    booleans como transformar de false a true

--- 06_booleans.py
+++ 06_booleans.py
@@ -1,1 +1,10 @@
 is_single = True
+print(type(is_single))
+is_single = False
+print(is_single)
+# en esta parte se puede cambiar lo false a true colocando por delante el not 
+print(not True)
+print(not False)
+
+is_single = not is_single
+print(is_single)
