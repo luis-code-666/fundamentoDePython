f50c38561d6ace60056fcf8cc3ef7787253d0519
rockband_666 <rockband_666@hotmail.com>
2023-12-05 23:00:35 -0400

    Aqui escrivbi mis primeras variables

---
+++ 02_variables.py
@@ -0,0 +1,18 @@
+print("hola, vars")
+
+# esto es una variable  
+my_name = "Nicolas"
+print(my_name)
+
+
+# esto es una variable con un numero 
+my_age = 12
+print(my_age)
+
+my_name = "Santiago"
+print("aqui cambio",  my_name)
+
+
+my_name = input("Cual es mi nombre? ")
+
+print("Usando input", my_name)
