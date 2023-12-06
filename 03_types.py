009f11fd91b8efdffc0bcb9d7714fada5ff8664d
rockband_666 <rockband_666@hotmail.com>
2023-12-05 23:12:27 -0400

    tipos de datos que tenemos en python

---
+++ 03_types.py
@@ -0,0 +1,22 @@
+# string es una cadena de texto 
+my_name = "Nicolas"
+my_name = "Santiago"
+
+print('my_name =>', my_name)
+print (type(my_name))
+
+#int
+my_age = 12
+print(' my_age =>', my_age)
+print (type(my_age))
+
+# boolean
+is_single = True
+print(' is_single =>', is_single)
+print(type(is_single))
+
+# inputs
+
+my_age = input(' Cula es tu edad? ')
+print('my_age =>', my_age)
+print(type(my_age))
