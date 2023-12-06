d7bfba5b3564206b8de1a0444a8dd7aafb37d52c
rockband_666 <rockband_666@hotmail.com>
2023-12-06 09:53:11 -0400

    Reto de sacar el promedio de numero

---
+++ 05_numbers.py
@@ -0,0 +1,13 @@
+# es buena practica colocar nombres que sean para identificar que estamos colocando 
+lives = 3
+print (type(lives))
+
+lives =  12 + 15
+print (lives)
+
+# reto de calcular el prmedio de mis tres variables 
+var_enero = int(input("coloque su gasto de Enero => "))
+var_febrero = int(input("coloque su gasto de Febrero => "))
+var_marzo = int(input("coloque su gasto de Marzo => "))
+promediio_meses = (var_enero + var_febrero + var_marzo)/3
+print (promediio_meses)
