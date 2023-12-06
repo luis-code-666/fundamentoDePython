8792fa2ffd61627da8dfa0f914a6b5305f5fc20d
rockband_666 <rockband_666@hotmail.com>
2023-12-06 10:59:44 -0400

    calculos aritmeticos y formas de calcular

---
+++ 08_arith.py
@@ -0,0 +1,15 @@
+print(10+10)
+print(10-10)
+print(10*2)
+print(10/2)
+print(10%2) #tiene residuo es 0
+print(10/3)
+print(10%3) #tiene residuo que sera 1  
+print(10//3) #en este medio se saca el entero de la divisiones la precicion decimal 
+print(2**3)
+print( 2**3 + 3-7 / 1 // 4 )
+print( (2**3) + 3-((7 / 1 )// 4) ) #este es el proceso como se resuelve 
+
+
+print('Hola' + ' Mundo')
+print('Hola' * 3)
