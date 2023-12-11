5b687666181de16e4f5bd2bea09de4454b07dd65
rockband_666 <rockband_666@hotmail.com>
2023-12-11 12:20:44 -0400

    como utilizar el AND y OR para funciones en un IF

---
+++ 11_logic.py
@@ -0,0 +1,24 @@
+print("AND")
+print('True and True => ', True and True)
+print('True and True => ', True and False)
+print('True and True => ', False and True)
+print('True and True => ', False and False)
+
+print(10 > 5 and 5 < 10)
+
+stock = input('Ingrese el numero de stock => ')
+stock = int(stock)
+
+print(stock >= 100 and stock <=10000)
+
+
+print("*" * 10)
+print("OR")
+print('True or True => ', True or True)
+print('True or True => ', True or False)
+print('True or True => ', False or True)
+print('True or True => ', False or False)
+
+rol = input('Digita el rol => ')
+
+print(rol == 'admin' or rol == 'user')
