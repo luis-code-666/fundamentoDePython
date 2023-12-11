44fd4feb1d1a2f51a38bbdb1b5e0a9be4667d483
rockband_666 <rockband_666@hotmail.com>
2023-12-11 12:01:49 -0400

    comparaciones de menor que maypr que y como usar el igual

---
+++ 09_comparators.py
@@ -0,0 +1,31 @@
+#> esto se usa mayor igual qeu 
+print(7>3)
+print(3>7)
+print(7>3)
+#< esto se usa menor igual qeu 
+print(7<3)
+print(3<7)
+print(7<3)
+#>= esto se usa mayor igual igual qeu 
+print(7>=3)
+print(3>=7)
+print(7>=3)
+#<= esto se usa menor igual igual qeu 
+print(7<3)
+print(3<7)
+print(7<3)
+
+# == es igual a que 
+print(7==7)
+print(5==2)
+
+# != es diferente a que 
+print(7!=7)
+print(5!=2)
+
+print("Apple" == "Apple")
+print("Apple" == "apple")
+print("1" == 1)
+
+age = 18
+print(age >= 18)
