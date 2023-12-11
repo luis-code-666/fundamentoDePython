fefc898c7b98bf02b028682600b00545ddbe8ca1
rockband_666 <rockband_666@hotmail.com>
2023-12-11 12:47:26 -0400

    como usar el if, elif y else

---
+++ 13_if.py
@@ -0,0 +1,25 @@
+if True:
+    print("deberia ejecutarse")
+
+if False:
+    print("Nunca se ejecuta")
+
+pet = input(' Cual es tu mascota favorita? ')
+if pet == 'perro':
+    print("genial tienes buenos gusto")
+elif pet == 'gato':
+    print("espero tengas suerte")
+elif pet == 'pez':
+    print("eres lo maximo")
+else:
+    print("no tienes ninguna mascota interesante")
+
+'''
+    
+stock = int(input('Digite el stock => '))
+
+if stock >= 100 and stock <=1000:
+    print("el stock es correcto")
+else:
+    print("el stock es incorrecto")
+'''
